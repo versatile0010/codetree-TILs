3f3e5b9a1e0c2c409d0e195179fca90db9ccc49f
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 01:14:22 +0900

    Title: 2021년 날짜의 유무, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231216/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(stk.nextToken());
+        int D = Integer.parseInt(stk.nextToken());
+        System.out.println(f(M, D));
+    }
+    public static String f(int m, int d){
+        if(m < 0 || m > 12) return "No";
+        if(m == 2 && (1 <= d && d <= 28)) return "Yes";
+        if(1<= d && d <= 31) return "Yes";
+        return "No"; 
+    }
+}
