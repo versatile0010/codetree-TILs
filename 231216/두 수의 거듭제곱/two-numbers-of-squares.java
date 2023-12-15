9853be215b6b036fc8b70619d6052218429f639a
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 00:33:26 +0900

    Title: 두 수의 거듭제곱, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231216/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(stk.nextToken());
+        int b = Integer.parseInt(stk.nextToken());
+
+        System.out.println(Math.pow(a, b));
+    }
+}
