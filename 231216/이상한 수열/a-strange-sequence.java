fd087add845b3af8d056727a858a5fdf3c957119
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 20:10:30 +0900

    Title: 이상한 수열, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231216/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(f(n));
+    }
+
+    public static int f(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        return f(n/3) + f(n-1);
+    }
+}
