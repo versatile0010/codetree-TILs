c8e6f379fe1e77356b6554189baa5a3ddbd490ee
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 16:34:38 +0900

    Title: 두 정수에 대한 연산값, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231216/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        IntWrapper a = new IntWrapper(Integer.parseInt(stk.nextToken()));
+        IntWrapper b = new IntWrapper(Integer.parseInt(stk.nextToken()));
+
+        f(a,b);
+
+        System.out.println(a.value+" "+b.value);
+    }
+
+    public static void f(IntWrapper a, IntWrapper b){
+        if(b.value > a.value){
+            f(b, a);
+            return;
+        }
+        a.setValue(a.value + 25);
+        b.setValue(b.value*2);
+
+    }
+
+    public static class IntWrapper{
+        int value;
+        public IntWrapper(int value){
+            this.value = value;
+        }
+        public void setValue(int value){
+            this.value = value;
+        }
+    }
+}
