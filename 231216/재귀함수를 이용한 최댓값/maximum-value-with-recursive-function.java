07199f0ee8c661e837383aa121781fc3f17b051e
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 20:02:19 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 231216/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
+++ 231216/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     static int [] a;
-    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -15,9 +14,6 @@ public class Main {
         System.out.println(f(n-1));
     }
     public static int f(int n){
-        if(n==0){
-            return a[0];
-        }
-        return Math.max(f(n-1), a[n]);
+        return n==0 ? a[0] : Math.max(f(n-1), a[n]);
     }
 }
