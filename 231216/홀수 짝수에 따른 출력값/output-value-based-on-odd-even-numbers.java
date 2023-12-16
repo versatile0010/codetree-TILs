9e4399bffabc6c3500c00a2a3cf14a1b4d67e862
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 19:55:24 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231216/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
+++ 231216/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println(f(n));
     }
     public static int f(int n){
-        if(n <= 1) return n;
+        if(n <= 0) return 0;
         if(n%2 == 0) return n + f(n/2);
         return n + f(n -2);
     }
