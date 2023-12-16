89aaf03cc9403266289dcd34d0f13dd30856a3ee
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 19:42:37 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 134ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231216/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(f(n));
+    }
+    public static int f(int n){
+        if(n==1 || n==2) return 1;
+        return f(n-1) + f(n-2);    
+    }
+}
