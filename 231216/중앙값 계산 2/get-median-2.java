66bab97528514127792fc42a1912d36e17badc51
JaeHyeon Lee <wogusn@gmail.com>
2023-12-16 21:44:20 +0900

    Title: 중앙값 계산 2, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231216/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int [] a = new int[n+1];
+        for(int i = 1; i <= n ; i++){
+            a[i] = Integer.parseInt(stk.nextToken());
+            if(i%2==1){
+                Arrays.sort(a, 1, i);
+                System.out.print(a[i/2+1]+" ");
+            }
+        }
+    }
+}
