9802b0f7027c98475002dd158ba9bbf91a9c694d
JaeHyeon Lee <wogusn@gmail.com>
2023-12-17 16:06:59 +0900

    Title: 요일 맞추기, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231217/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int m1 = Integer.parseInt(stk.nextToken());
+        int d1 = Integer.parseInt(stk.nextToken());
+        int m2 = Integer.parseInt(stk.nextToken());
+        int d2 = Integer.parseInt(stk.nextToken());
+
+        int [] t = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int srcDays = d1;
+        for(int i = 0 ; i < m1-1; i++){
+            srcDays += t[i];
+        }
+        
+        int destDays = d2;
+        for(int i = 0 ; i < m2-1; i++){
+            destDays += t[i];
+        }        
+
+        int dayDiff = Math.abs(destDays-srcDays+1);
+
+        String [] table = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        System.out.println(table[dayDiff%7]);
+    }
+}
