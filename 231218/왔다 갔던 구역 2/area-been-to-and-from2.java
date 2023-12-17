6c8181c2c9f113bdc78f178d163671e573273810
JaeHyeon Lee <wogusn@gmail.com>
2023-12-18 08:13:06 +0900

    Title: 왔다 갔던 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231218/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 231218/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -7,28 +7,34 @@ public class Main {
         int n = Integer.parseInt(br.readLine()); 
 
         int [] arr = new int [2001];
+        int [] st = new int[n];
+        int [] end = new int[n];
 
-        int prev = 1000;
+        int cur = 1000;
         for(int i = 0 ; i < n ; i ++){
             StringTokenizer stk = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(stk.nextToken());
             String op = stk.nextToken();
-            int j = 0;
+            
             if("R".equals(op)){
-                for(j = prev; j < prev+x; j++){
-                    arr[j]++;
-                }
-            } else { 
-                for(j = prev; j >= prev-x; j--){
-                    arr[j]++;
-                }
+                st[i] = cur;
+                end[i] = cur+x;
+                cur += x;
+            } else {
+                st[i] = cur-x;
+                end[i] = cur;
+                cur -= x;
             }
-            prev = j;
-            //System.out.println(prev-1000);
+            st[i] += 1000;
+            end[i] += 1000;
         }
-
-        int cnt = 0 ;
-        for(int i = 0; i <= 2000; i++){
+        for(int i = 0; i < n; i++){
+            for(int j = st[i]; j < end[i]; j++){
+                arr[j]++;
+            }
+        }
+        int cnt = 0;
+        for(int i = 0 ; i <= 2000; i++){
             if(arr[i]>=2){
                 cnt++;
             }
