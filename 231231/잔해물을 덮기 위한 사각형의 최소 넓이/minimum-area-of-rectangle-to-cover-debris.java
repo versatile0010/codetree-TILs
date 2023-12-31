8589a0ab0dd9db273792b3103f05f96cb9a1aa44
JaeHyeon Lee <wogusn@gmail.com>
2023-12-31 18:03:08 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231231/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int [][] graph = new int[2001][2001];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i = 0 ; i < 2; i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(stk.nextToken()) + 1000;
+            int y1 = Integer.parseInt(stk.nextToken()) + 1000;
+            int x2 = Integer.parseInt(stk.nextToken()) + 1000;
+            int y2 = Integer.parseInt(stk.nextToken()) + 1000;
+
+            for(int j = y1; j < y2; j++){
+                for(int k = x1; k < x2; k++){
+                    graph[j][k] = (i==0) ? 1 : 0; 
+                }
+            }
+        }
+
+        int maxX = -1;
+        int maxY = -1;
+        int minX = 2001;
+        int minY = 2001;
+        for(int i = 0 ; i <=2000; i++){
+            for(int j = 0 ; j <=2000; j++){
+                if(graph[i][j]==0) continue;
+                maxX = Math.max(maxX, j);
+                maxY = Math.max(maxY, i);
+                minX = Math.min(minX, j);
+                minY = Math.min(minY, i);
+            }
+        }
+        int ans = (maxX - minX + 1) * (maxY - minY + 1);
+        System.out.println(ans);
+    }
+}
