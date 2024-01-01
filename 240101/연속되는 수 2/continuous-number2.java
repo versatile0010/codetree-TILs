723ab1c7917203a50cd1c632e2f09755bcee8832
JaeHyeon Lee <wogusn@gmail.com>
2024-01-01 14:11:26 +0900

    Title: 연속되는 수 2, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240101/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [] arr = new int [n];
+        int ans = -1;
+        int cnt = 0;
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = 0 ; i < n; i++){
+            cnt++;
+            if(i==0 || arr[i] != arr[i-1]){
+                ans = Math.max(ans, cnt);
+                cnt = 0;
+            }
+        }
+        System.out.println(ans);
+    }
+}
