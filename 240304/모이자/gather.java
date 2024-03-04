4bffe6726191d7c78d766e0ce1ce1db8caba2191
Lee Jaehyeon <wogusn@gmail.com>
2024-03-04 20:26:37 +0900

    Title: 모이자, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240304/모이자/gather.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int [] a = new int[n];
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n ; i++){
+            a[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        int mini = Integer.MAX_VALUE;
+        for(int i = 0 ; i < n ; i++){
+            int x = 0;
+            for(int j = 0 ; j < n ; j++){
+                if( j == i) continue;
+                x += Math.abs(i - j) * a[j];
+            }
+            if ( x <= mini ) {
+                mini = x;
+            }
+        }
+        System.out.println(mini);
+    }
+}
