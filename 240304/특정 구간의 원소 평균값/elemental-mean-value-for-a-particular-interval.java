e81ba489039c5094c557b72b7a527c96a95205b3
Lee Jaehyeon <wogusn@gmail.com>
2024-03-04 23:18:31 +0900

    Title: 특정 구간의 원소 평균값, Time: 109ms, Memory: 9MB, Status: Passed - CodeTree

--- 240304/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 240304/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -14,10 +14,6 @@ public class Main {
         int cnt = 0;
         for(int i = 0 ; i < n; i++){
             for(int j = i; j < n ; j++){
-                if(i==j) {
-                    cnt++;
-                    continue;
-                }
                 int sum = 0;
                 for(int k = i; k <= j; k++){
                     sum += a[k];
