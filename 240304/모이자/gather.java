627e296c369c55a03458144b3dc9809dc50327ac
Lee Jaehyeon <wogusn@gmail.com>
2024-03-04 20:27:06 +0900

    Title: 모이자, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 240304/모이자/gather.java
+++ 240304/모이자/gather.java
@@ -16,7 +16,6 @@ public class Main {
         for(int i = 0 ; i < n ; i++){
             int x = 0;
             for(int j = 0 ; j < n ; j++){
-                if( j == i) continue;
                 x += Math.abs(i - j) * a[j];
             }
             if ( x <= mini ) {
