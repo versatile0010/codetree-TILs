d00c4ed81efbd1c3dfaca7db752b2f1451de7f13
Lee Jaehyeon <wogusn@gmail.com>
2024-03-06 22:37:05 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 2103ms, Memory: 15MB, Status: Time Limit Exceed - CodeTree

---
+++ 240306/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [] a = new int[n];
+        int [] b = new int[n];
+        boolean [] used = new boolean [n];
+
+        for(int i = 0 ; i < n ; i ++){
+            b[i] = Integer.parseInt(br.readLine());
+        }
+        int bIdx = 0;
+        int aIdx = 0;
+        for(int x = 1; x <= 2*n; x++){
+            if(!isContains(x, b)){
+                a[aIdx++] = x;
+            }
+        }
+
+        Arrays.sort(a);
+        int score = 0;
+        for(int i = 0; i < n ; i++){
+            int bx = b[i];
+            for(int j = 0 ; j < n ; j++){
+                int ax = a[j];
+                if(!used[j] && ax > bx){
+                    score++;
+                    used[j] = true;
+                    break;
+                }
+            }
+        }
+        System.out.println(score);
+    }
+    public static boolean isContains(int x, int [] b){
+        for(int ele : b){
+            if( x == ele) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
