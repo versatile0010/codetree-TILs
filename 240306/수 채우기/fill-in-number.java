7b850d4ed3adb95638d67358e6c3642e81c5f431
Lee Jaehyeon <wogusn@gmail.com>
2024-03-06 21:53:15 +0900

    Title: 수 채우기, Time: 106ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240306/수 채우기/fill-in-number.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = n / 5;
+        n %= 5;
+        cnt += n / 2;
+
+        System.out.println(cnt);
+    }
+}
