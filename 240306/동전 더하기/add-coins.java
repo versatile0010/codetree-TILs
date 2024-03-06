09f2bba1475e441dff1e4638b38a1bb53d7fe378
Lee Jaehyeon <wogusn@gmail.com>
2024-03-06 20:32:15 +0900

    Title: 동전 더하기, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240306/동전 더하기/add-coins.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        int k = Integer.parseInt(stk.nextToken());
+        int [] coins = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int cnt = 0;
+        for(int i = n-1; i >= 0 ; i--){
+            if(k == 0) break;
+            int r = k / coins[i];
+            k -= r*coins[i];
+            cnt += r;
+        }
+        System.out.println(cnt);
+    }
+}
