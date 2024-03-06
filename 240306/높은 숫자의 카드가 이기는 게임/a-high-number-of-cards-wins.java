c5722ef003455a623ce04fb4dcf3ca0b24ab1ca6
Lee Jaehyeon <wogusn@gmail.com>
2024-03-06 22:41:27 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 344ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
+++ 240306/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -8,38 +8,29 @@ public class Main {
         int [] a = new int[n];
         int [] b = new int[n];
         boolean [] used = new boolean [n];
-
+        HashSet<Integer> hs = new HashSet<>();
         for(int i = 0 ; i < n ; i ++){
             b[i] = Integer.parseInt(br.readLine());
+            hs.add(b[i]);
         }
         int bIdx = 0;
         int aIdx = 0;
         for(int x = 1; x <= 2*n; x++){
-            if(!isContains(x, b)){
+            if(!hs.contains(x)){
                 a[aIdx++] = x;
             }
         }
 
+        Arrays.sort(a);
+        Arrays.sort(b);
         int score = 0;
-        for(int i = 0; i < n ; i++){
-            int bx = b[i];
-            for(int j = 0 ; j < n ; j++){
-                int ax = a[j];
-                if(!used[j] && ax > bx){
-                    score++;
-                    used[j] = true;
-                    break;
-                }
+        int j = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(j < n && a[i] > b[j]) {
+                score++;
+                j++;
             }
         }
         System.out.println(score);
-    }
-    public static boolean isContains(int x, int [] b){
-        for(int ele : b){
-            if( x == ele) {
-                return true;
-            }
-        }
-        return false;
     }
 }
