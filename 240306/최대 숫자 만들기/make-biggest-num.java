e54407d1ed688570267025f65d85b61870ee73cd
Lee Jaehyeon <wogusn@gmail.com>
2024-03-06 21:48:26 +0900

    Title: 최대 숫자 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240306/최대 숫자 만들기/make-biggest-num.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer [] arr = new Integer[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr, (a, b)->{
+            int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+            int ba = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+            if(ba >= ab){
+                return 1;
+            }
+            return -1;
+        });
+        for(int i = 0 ; i < n ; i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
