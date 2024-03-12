eeb0dd6f6905adee072bcdfb526c233bb6805aab
Lee Jaehyeon <wogusn@gmail.com>
2024-03-12 22:44:00 +0900

    Title: 독서실의 거리두기 3, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240312/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int [] arr = new int[n];
+        for(int i = 0 ; i < n; i++){
+            arr[i] = str.charAt(i) - '0';
+        }
+
+        // 가장 먼 두 위치를 표시하자.
+        int prev = -1;
+        int diff = -1;
+        int st = -1;
+        int end = -1;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == 1) {
+                if(prev != -1) {
+                    int dist = i - prev;
+                    if(diff <= dist) {
+                        diff = dist;
+                        st = prev;
+                        end = i;
+                    }
+                }
+            }
+            prev = i;
+        }
+        // 가장 먼 거리를 가진 좌표는 st 와 prev 이다.
+        arr[(end + st)/2] = 1;
+        int ans = n;
+        prev = -1;
+        for(int i = 0 ; i < n ; i++) {
+            if(arr[i] == 1) {
+                int dist = i - prev + 1;
+                ans = Math.min(ans, dist);
+            }
+            prev = i;
+        }
+        System.out.println(ans);
+    }
+}
