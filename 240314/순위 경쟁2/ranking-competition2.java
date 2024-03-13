c0d675586f4f4824322f16876632c15d34f7d1dd
Lee Jaehyeon <wogusn@gmail.com>
2024-03-14 00:44:24 +0900

    Title: 순위 경쟁2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240314/순위 경쟁2/ranking-competition2.java
+++ 240314/순위 경쟁2/ranking-competition2.java
@@ -6,10 +6,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk;
         int n = Integer.parseInt(br.readLine());
-        int [] a = new int [n];
-        int [] b = new int [n];
+        int [] a = new int [n+1];
+        int [] b = new int [n+1];
 
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 1 ; i < n ; i++){
             stk = new StringTokenizer(br.readLine());
             String op = stk.nextToken();
             int score = Integer.parseInt(stk.nextToken());
@@ -21,15 +21,15 @@ public class Main {
                 a[i] = i == 0 ? 0 : a[i-1];
             }
         }
-        int ans = n == 1 ? 0 : 1;
-        for(int i = 1 ; i < n ; i++){
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
             if(
-                (a[i-1] == b[i-1] && a[i] > b[i]) ||
-                (a[i-1] == b[i-1] && a[i] < b[i]) ||
-                (a[i-1] > b[i-1] && a[i] == b[i]) ||
-                (a[i-1] > b[i-1] && a[i] < b[i]) ||
-                (a[i-1] < b[i-1] && a[i] == b[i]) ||
-                (a[i-1] < b[i-1] && a[i] > b[i])
+                (a[i] == b[i] && a[i+1] > b[i+1]) ||
+                (a[i] == b[i] && a[i+1] < b[i+1]) ||
+                (a[i] > b[i] && a[i+1] == b[i+1]) ||
+                (a[i] > b[i] && a[i+1] < b[i+1]) ||
+                (a[i] < b[i] && a[i+1] == b[i+1]) ||
+                (a[i] < b[i] && a[i+1] > b[i+1])
             ) ans++;
         }
         System.out.println(ans);
