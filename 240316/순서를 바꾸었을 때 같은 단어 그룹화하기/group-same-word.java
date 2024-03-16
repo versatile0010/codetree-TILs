18730d43c0deed0fc239af19594c58950bb73846
Lee Jaehyeon <wogusn@gmail.com>
2024-03-16 22:13:03 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 433ms, Memory: 20MB, Status: Passed - CodeTree

--- 240316/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.java
+++ 240316/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.java
@@ -7,32 +7,18 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         HashMap<String, Integer> hm = new HashMap<>();
         while(n-- > 0){
-            String input = br.readLine();
-            String t = f(input);
-            //System.out.println(t);
-            hm.put(t, hm.getOrDefault(t, 0) + 1);
+            String input = sortString(br.readLine());
+            hm.put(input, hm.getOrDefault(input, 0) + 1);
         }
-        List<String> keyList = new ArrayList<>(hm.keySet());
-        Collections.sort(keyList, (String a, String b) -> {
-            return hm.get(b) - hm.get(a);
-        });
-        System.out.println(hm.get(keyList.get(0)));
+        int ans = 0;
+        for(String key : hm.keySet()){
+            ans = Math.max(ans, hm.get(key));
+        }
+        System.out.println(ans);
     }
-    public static String f(String str){
-        
-        char [] charArray = str.toCharArray();
-        Arrays.sort(charArray);
-        str = new String(charArray);
-
-        Map<Character, Integer> hm = new HashMap<>();
-        for(int i = 0 ; i < str.length(); i++){
-            char x = str.charAt(i);
-            hm.put(x, hm.getOrDefault(x, 0) + 1);
-        }
-        StringBuilder sb = new StringBuilder();
-        for(char key : hm.keySet()){
-            sb.append(String.valueOf(key) + hm.get(key));
-        }
-        return sb.toString();
+    public static String sortString(String input){
+        char[] arr = input.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
     }
 }
