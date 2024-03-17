c71783ed4f01a743e74bcbaf867db134239d0e0c
Lee Jaehyeon <wogusn@gmail.com>
2024-03-17 13:16:17 +0900

    Title: 겹치지 않게 선분 고르기, Time: 3040ms, Memory: 122MB, Status: Time Limit Exceed - CodeTree

--- 240317/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240317/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -20,16 +20,14 @@ public class Main {
             int x2 = Integer.parseInt(stk.nextToken());
             lines[i] = new Pair(x1, x2);
         }
-        for(int i = 1 ; i <= n; i++){
-            dfs(0, i);
-        }
+        dfs(0);
         System.out.println(ans);
     }
-    public static void dfs(int depth, int choose){
-        if(depth == choose){
-            // 선택한 choose 개의 선분으로 뽑을 수 있는 최대 선분의 수를 구한다.
-            Pair[] pairs = new Pair[choose];
-            for(int i = 0 ; i < choose; i++){
+    public static void dfs(int depth){
+        if(depth == n){
+            // 선택한 n 개의 선분으로 뽑을 수 있는 최대 선분의 수를 구한다.
+            Pair[] pairs = new Pair[n];
+            for(int i = 0 ; i < n; i++){
                 pairs[i] = lines[i];
             }
             Arrays.sort(pairs, (Pair a, Pair b)-> {
@@ -37,7 +35,7 @@ public class Main {
             });
             int cnt = 1;
             Pair prev = pairs[0];
-            for(int i = 1 ; i < choose ; i++){
+            for(int i = 1 ; i < n ; i++){
                 if(pairs[i].x1 > prev.x2){
                     cnt++;
                     prev = pairs[i];
@@ -50,7 +48,7 @@ public class Main {
                 if(!visited[i]){
                     visited[i] = true;
                     choice[depth] = i;
-                    dfs(depth+1, choose);
+                    dfs(depth+1);
                     visited[i] = false;
                 }
             }
