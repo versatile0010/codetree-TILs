2236b767c493e4d88c2e18984dde5338425139c3
Lee Jaehyeon <wogusn@gmail.com>
2024-03-17 15:54:39 +0900

    Title: xor 결과 최대 만들기, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240317/xor 결과 최대 만들기/max-of-xor.java
+++ 240317/xor 결과 최대 만들기/max-of-xor.java
@@ -6,6 +6,7 @@ public class Main {
     public static int n;
     public static int m;
     public static int [] arr;
+    public static int [] base;
     public static boolean [] used;
     public static int ans;
 
@@ -16,31 +17,32 @@ public class Main {
         m = Integer.parseInt(nm[1]);
 
         arr = new int[n];
+        base = new int[n];
         used = new boolean[n+1];
 
-        dfs(0, 1);
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < n ; i++){
+            base[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        dfs(0, 0);
         System.out.println(ans);
     }
 
     public static void dfs(int depth, int start){
         if(depth == m){
             // xor 계산 후 ans 갱신
-            int calc = arr[0];
-            if(m == 1) {
-                calc = 0;
-            } else {
-                for(int i = 1 ; i < m; i++){
-                    calc ^= arr[i];
-                }
+            int calc = base[0];
+            for(int i = 1 ; i < m; i++){
+                calc ^= base[i];
             }
-
             ans = Math.max(calc, ans);
             return;
         }
-        for(int i = start; i <= n; i++){
+        for(int i = start; i < n; i++){
             if(!used[i]){
                 used[i] = true;
-                arr[depth] = i;
+                arr[depth] = base[i];
                 dfs(depth+1, start+1);
                 used[i] = false;
             }
