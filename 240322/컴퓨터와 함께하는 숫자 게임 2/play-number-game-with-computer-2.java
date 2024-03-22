e635a5756f7ee205a98d9b1106dd178c91cd17f7
Lee Jaehyeon <wogusn@gmail.com>
2024-03-22 21:58:45 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 147ms, Memory: 8MB, Status: Passed - CodeTree

--- 240322/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
+++ 240322/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -2,19 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static long m;
-    public static int a,b;
+    public static long m,a,b;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
         m = Long.parseLong(stk.nextToken());
 
         stk = new StringTokenizer(br.readLine());
-        a = Integer.parseInt(stk.nextToken());
-        b = Integer.parseInt(stk.nextToken());
-        int mini = Integer.MAX_VALUE;
-        int maxi = Integer.MIN_VALUE;
-        for(int x = a; x <= b; x++){
+        a = Long.parseLong(stk.nextToken());
+        b = Long.parseLong(stk.nextToken());
+        long mini = Integer.MAX_VALUE;
+        long maxi = Integer.MIN_VALUE;
+        for(long x = a; x <= b; x++){
             int cnt = binarySearch(x);
             mini = Math.min(cnt, mini);
             maxi = Math.max(cnt, maxi);
